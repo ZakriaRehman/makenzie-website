52254d359dcbf73eae24034b4aaa4ffa926df654
Maria Aftab <mkiyani012@gmail.com>
2025-12-27 01:15:01 +0500

    Fixed Calendly Meeting Name

--- src/config/calendly.ts
+++ src/config/calendly.ts
@@ -1,7 +1,7 @@
 // Calendly Configuration
 // Replace this URL with your actual Calendly scheduling link
 
-export const CALENDLY_URL = 'https://calendly.com/makenzie-info/30min'
+export const CALENDLY_URL = 'https://calendly.com/makenzie-info/new-meeting'
 
 // To get your Calendly URL:
 // 1. Log into calendly.com
